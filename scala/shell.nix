c5c0e54eadee66d5c60a8a9b1389766468ae7a15
truecrunchyfrog <javaveryhot@protonmail.ch>
2025-06-09 19:05:08 +0200

    scala: Add shell.nix and .envrc

---
+++ scala/shell.nix
@@ -0,0 +1,12 @@
+let
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-25.05";
+  pkgs = import nixpkgs { config = {}; overlays = []; };
+in
+
+pkgs.mkShellNoCC {
+  packages = with pkgs; [
+      exercism
+      scala
+      sbt
+  ];
+}
